fa818561c623fbfafaa02e205d1ffe094df4f17e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-28 08:14:25 +0200

    Dodanie testów MarkdownFormatter

--- src/01_Fundamentals/06_TestingStrings/MarkdownFormatter.cs
+++ src/01_Fundamentals/06_TestingStrings/MarkdownFormatter.cs
@@ -19,3 +19,26 @@ public class MarkdownFormatter
 
 
 // TODO: Dodaj kolejne formatowania Italic, Header i Hiperlink
+
+public class MarkdownFormatterTests
+{
+    private const string DoubleAsterix = "**";
+    
+    [Theory]
+    [InlineData("a")]
+    [InlineData("Lorem ipsum")]
+    [InlineData("Lorem*ipsum")]
+    public void FormatAsBold_NotEmptyContent_ReturnsAroundedByDoubleAsterix(string content)
+    {
+        // Arrange
+        var formatter = new MarkdownFormatter();
+        
+        // Act
+        var result = formatter.FormatAsBold(content);
+        
+        // Assert
+        Assert.StartsWith(DoubleAsterix, result);
+        Assert.Contains(content, result);
+        Assert.EndsWith(DoubleAsterix, result);
+    }
+}
