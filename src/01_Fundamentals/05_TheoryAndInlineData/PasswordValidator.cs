c823e695478f54e6a2e010e8b1e0127624d978be
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-28 08:14:10 +0200

    Dodanie testów PasswordValidator

--- src/01_Fundamentals/05_TheoryAndInlineData/PasswordValidator.cs
+++ src/01_Fundamentals/05_TheoryAndInlineData/PasswordValidator.cs
@@ -13,15 +13,44 @@ Hasło jest ważne, jeśli:
 - nie może być puste ("Password cannot be empty")
 - zawiera co najmniej 8 znaków ("Password must be at least 8 characters long")
 - zawiera przynajmniej jedną cyfrę ("Password must contain at least one digit")
-- zawiera przynajmniej jedną wielką literę ("Password must contain at least one digit")
+- zawiera przynajmniej jedną wielką literę ("Password must contain at least one big letter")
 */
 
 public class PasswordValidator
 {
     public PasswordValidationResult Validate(string password)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(password))
+            return new PasswordValidationResult(false, "Password cannot be empty");
+        
+        if (password.Length < 8)
+            return new PasswordValidationResult(false, "Password must be at least 8 characters long");
+        
+        return new PasswordValidationResult(true, string.Empty);
     }
 }
 
 public record PasswordValidationResult(bool IsValid, string? ErrorMessage);
+
+public class PasswordValidatorTests
+{
+    [Theory]
+    [InlineData("", false, "Password cannot be empty")]
+    [InlineData("abc", false, "Password must be at least 8 characters long")]
+    [InlineData("012345678", false, "Password must contain at least one digit")]
+    [InlineData("abcdefgh9", false, "Password must contain at least one big letter")]
+    [InlineData("Abcdefgh9", true, "")]
+    public void Validate_PassPassword_ReturnsExpectedResult(string password, bool expectedIsValid,
+        string expectedErrorMessage)
+    {
+        // Arrange
+        var validator = new PasswordValidator();
+        
+        // Act
+        var result = validator.Validate(password);
+        
+        // Assert
+        Assert.Equal(expectedIsValid, result.IsValid);
+        Assert.Equal(expectedErrorMessage, result.ErrorMessage);
+    }
+}
