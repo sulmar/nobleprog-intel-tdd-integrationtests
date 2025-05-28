9ea3520247c53957ce174b0ac0cfc57a78f3a77f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-05-28 16:55:58 +0200

    Dodanie Stub Tests

--- src/02_TestDoubles/02_Stub/StubTests/TrackingService.cs
+++ src/02_TestDoubles/02_Stub/StubTests/TrackingService.cs
@@ -2,18 +2,65 @@ using System.Text.Json;
 
 namespace StubTests;
 
+public interface IFileReader
+{
+    string ReadAllText(string path); 
+}
+
+public class RealFileReader : IFileReader
+{
+    public string ReadAllText(string path) => File.ReadAllText(path);
+}
+
+public class StubValidFileReader : IFileReader
+{
+    public string ReadAllText(string _)
+    {
+        var location = new Location(21.01, 52.01);
+
+        var json = JsonSerializer.Serialize(location);
+
+        return json;
+    }
+}
+
+public class StubInvalidFileReader : IFileReader
+{
+    public string ReadAllText(string path) => "a";
+}
+
+public class StubEmptyFileReader : IFileReader
+{
+    public string ReadAllText(string path) => String.Empty;
+}
+
 public class TrackingService
 {
+    private readonly IFileReader _fileReader;
+
+    public TrackingService()
+        : this(new RealFileReader())
+    {
+        
+    }
+    
+    public TrackingService(IFileReader fileReader)
+    {
+        _fileReader = fileReader;
+    }
+
     public Location Get()
     {
-        string json = File.ReadAllText("tracking.txt");
+        string json = _fileReader.ReadAllText("tracking.json");
 
-        Location location = JsonSerializer.Deserialize<Location>(json);
-
-        if (location == null)
+        try
+        {
+            return JsonSerializer.Deserialize<Location>(json);
+        }
+        catch (JsonException e)
+        {
             throw new ApplicationException("Error parsing the location");
-
-        return location;
+        }
     }
 }
 
--- src/02_TestDoubles/02_Stub/StubTests/UnitTest1.cs
+++ src/02_TestDoubles/02_Stub/StubTests/UnitTest1.cs
@@ -1,10 +1,31 @@
 namespace StubTests;
 
-public class UnitTest1
+public class TrackingServiceTests
 {
     [Fact]
-    public void Test1()
+    public void Get_ValidFile_ReturnsLocation()
     {
+        // Arrange
+        TrackingService service = new TrackingService(new StubValidFileReader());
+        
+        // Act
+        var location = service.Get();
 
+        // Assert
+        Assert.NotNull(location);
+        Assert.Equal(21.01, location.Latitude);
+        Assert.Equal(52.01, location.Longitude);
+
+    }
+
+    [Fact]
+    public void Get_InvalidFile_ThrowsException()
+    {
+        // Arrange
+        TrackingService service = new TrackingService(new StubInvalidFileReader());
+        
+        // Act & Assert
+        Assert.Throws<ApplicationException>( () => service.Get());
+        
     }
 }
